bd2df5e142f7995b0546f19c75b1b753285010d4
BValeo <vibobrovko@gmail.com>
2017-02-17 21:37:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+
+  socket.on('new message', function (data) {
+    socket.emit('new message', {
+      message: data
+    });
+  });
+});
