8ab3b928d9e61a6422f8775c9544b2738b0cb0b1
BValeo <vibobrovko@gmail.com>
2017-05-14 16:56:20 +0300

    fix list users4

--- index.js
+++ index.js
@@ -51,17 +51,7 @@ io.on('connection', function (socket) {
 	socket.on('new message', function(to, from, data, time, isMine, type){
 		console.log('==========new MESSAGE===============');
 
-		var id = "null";
-
-		for(var key in  users){
-			if(key == to) id = users[key];
-			break;
-		}
-
-		console.log('1 ' + to + ' == ' + id);
-		console.log('2 ' + to + ' == ' + users[to])
-
-		io.to(id).emit('new message', {
+		io.to(users[to]).emit('new message', {
 			'username' : from,
 			'message' : data,
 			'time' : time,
